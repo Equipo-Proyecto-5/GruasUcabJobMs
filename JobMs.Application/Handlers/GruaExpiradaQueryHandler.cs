8b67ee19acdc276e2bca9e77d92540afb4adb242
Gamartinez25 <gamartinez.21@est.ucab.edu.ve>
2024-12-27 20:09:19 -0400

    Job para inactivar una grua por localizacion expirada

---
+++ JobMs.Application/Handlers/GruaExpiradaQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobMs.Application.Queries;
+using JobMs.Core.Services;
+using MediatR;
+
+namespace JobMs.Application.Handlers
+{
+    public class GruaExpiradaQueryHandler : IRequestHandler<GruaExpiradaQuery>
+    {
+        private readonly IProvidersMsService ProvidersMsService;
+
+        public GruaExpiradaQueryHandler(IProvidersMsService providersMsService)
+        {
+            ProvidersMsService = providersMsService;
+        }
+        public async Task Handle(GruaExpiradaQuery request, CancellationToken cancellationToken)
+        {
+            await ProvidersMsService.LocalizacionGruaExpirada();
+
+        }
+    }
+}
---
+++ JobMs.Core/Services/IProvidersMsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobMs.Core.Services
+{
+    public interface IProvidersMsService
+    {
+        Task LocalizacionGruaExpirada();
+    }
+}
